46dd8ae2809dcee3d3b918b12eab2f9805ff8a69
Andrey Shestakov <andrey230781.an@gmail.com>
2023-04-05 20:15:43 +0300

    first commit

---
+++ src/js/__tests__/health.test.js
@@ -0,0 +1,29 @@
+import sortedHealth from '../health';
+
+test('return correct value', () => {
+  const heroes = sortedHealth([
+    { name: 'мечник', health: 10 },
+    { name: 'лучник', health: 100 },
+    { name: 'маг', health: 80 },
+  ]);
+  const result = [
+    { name: 'лучник', health: 100 },
+    { name: 'маг', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(heroes).toEqual(result);
+});
+
+test('return incorrect value', () => {
+  const heroes = sortedHealth([
+    { name: 'мечник', health: 10 },
+    { name: 'лучник', health: 100 },
+    { name: 'маг', health: 80 },
+  ]);
+  const result = [
+    { name: 'мечник', health: 10 },
+    { name: 'лучник', health: 100 },
+    { name: 'маг', health: 80 },
+  ];
+  expect(heroes).toBe(result);
+});
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import sortedHealth from './health.js';
+
+const arr = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+console.log(sortedHealth(arr));
